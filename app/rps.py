58b21cc8d6d78550efd963d7f17610ecc0fd59e2
mochimochi11010 <92482477+mochimochi11010@users.noreply.github.com>
2025-11-06 20:01:46 -0500

    Testing

--- app/rps.py
+++ app/rps.py
@@ -3,25 +3,6 @@ import random
 
 VALID_OPTIONS = ["rock", "paper", "scissors"]
 
-# ASK USER FOR AN INPUT (R/P/S)
-
-user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
-print("USER:", user_choice)
-
-# VALIDATIONS
-
-if user_choice not in VALID_OPTIONS:
-    print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
-    # exit()
-    quit()
-
-# GENERATE RANDOM COMPUTER CHOICE
-
-computer_choice = random.choice(VALID_OPTIONS)
-print("COMP:", computer_choice)
-
-# DETERMINE THE WINNER
-
 def determine_winner(u,c):
     if u == "rock" and c == "rock":
         return "TIE GAME"
@@ -42,4 +23,25 @@ def determine_winner(u,c):
     elif u == "scissors" and c == "scissors":
         return "TIE GAME"
 
-determine_winner(user_choice, computer_choice)
+if __name__ == "__main__":
+
+    # ASK USER FOR AN INPUT (R/P/S)
+
+    user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
+    print("USER:", user_choice)
+
+    # VALIDATIONS
+
+    if user_choice not in VALID_OPTIONS:
+        print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+        # exit()
+        quit()
+
+    # GENERATE RANDOM COMPUTER CHOICE
+
+    computer_choice = random.choice(VALID_OPTIONS)
+    print("COMP:", computer_choice)
+
+    # DETERMINE THE WINNER
+
+    determine_winner(user_choice, computer_choice)
