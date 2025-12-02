0de5d9afb82cbf12e810388efd242d0ca12d151d
mochimochi11010 <92482477+mochimochi11010@users.noreply.github.com>
2025-12-01 20:02:04 -0500

    plz work

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,22 +1,25 @@
-
 # this is the "web_app/__init__.py" file...
 
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.rps_routes import rps_routes
 from web_app.routes.stocks_routes import stocks_routes
 
-#from web_app.routes.book_routes import book_routes
-#from web_app.routes.weather_routes import weather_routes
+# load variables in .env into environment
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
+
+    # read secret key from environment
+    app.config["SECRET_KEY"] = os.getenv("SECRET_KEY", "default_temp_key")
+
     app.register_blueprint(home_routes)
     app.register_blueprint(rps_routes)
     app.register_blueprint(stocks_routes)
-    #app.register_blueprint(book_routes)
-    #app.register_blueprint(weather_routes)
     return app
 
 if __name__ == "__main__":
