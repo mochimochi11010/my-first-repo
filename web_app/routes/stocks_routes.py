29e1ed0c3963ffa749beee1a1da7028229ecf662
mochimochi11010 <92482477+mochimochi11010@users.noreply.github.com>
2025-12-01 19:30:13 -0500

    bug fix

--- web_app/routes/stocks_routes.py
+++ web_app/routes/stocks_routes.py
@@ -3,7 +3,7 @@
 
 from flask import Blueprint, request, render_template, redirect, flash
 
-from app.stocks import fetch_stocks_data, format_usd
+from app.stocks import fetch_stocks_csv, to_usd
 
 stocks_routes = Blueprint("stocks_routes", __name__)
 
